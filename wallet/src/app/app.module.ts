106df112dd8a230923a071c49564f4a4b83bc96d
Adrien Mazet <adrien.mazet.56@gmail.com>
2022-11-09 18:09:06 +0100

    use angular material components in form

--- wallet/src/app/app.module.ts
+++ wallet/src/app/app.module.ts
@@ -1,15 +1,19 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
-import { TodoComponent } from './todo/todo.component';
 import { NewOperationReactiveFormComponent } from './new-operation-reactive-form/new-operation-reactive-form.component';
 import { NewOperationTemplateDrivenFormComponent } from './new-operation-template-driven-form/new-operation-template-driven-form.component';
+import { TodoComponent } from './todo/todo.component';
 
 @NgModule({
   declarations: [
@@ -20,11 +24,15 @@ import { NewOperationTemplateDrivenFormComponent } from './new-operation-templat
     NewOperationTemplateDrivenFormComponent,
   ],
   imports: [
+    MatSelectModule,
+    MatInputModule,
+    MatCardModule,
     MatIconModule,
     BrowserModule,
     MatToolbarModule,
     AppRoutingModule,
     MatButtonModule,
+    FormsModule,
     BrowserAnimationsModule,
   ],
   providers: [],
--- wallet/src/app/new-operation-template-driven-form/new-operation-template-driven-form.component.ts
+++ wallet/src/app/new-operation-template-driven-form/new-operation-template-driven-form.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-new-operation-template-driven-form',
   templateUrl: './new-operation-template-driven-form.component.html',
-  styleUrls: ['./new-operation-template-driven-form.component.css']
+  styleUrls: ['./new-operation-template-driven-form.component.css'],
 })
-export class NewOperationTemplateDrivenFormComponent implements OnInit {
+export class NewOperationTemplateDrivenFormComponent {
+  amount = 0;
+  selectedType = '';
+  // get if from service with observables
+  types = ['income', 'expense', 'saving'];
 
-  constructor() { }
-
-  ngOnInit(): void {
+  onSubmit() {
+    console.log(this.selectedType, this.amount);
+    this.amount = 0;
+    this.selectedType = '';
   }
-
 }
